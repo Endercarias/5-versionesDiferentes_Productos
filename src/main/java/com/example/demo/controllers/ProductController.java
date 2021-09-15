0e9ddcafa8c17ffbb669815c6962a29980c47437
Ender Carias <endercarias@gmail.com>
2021-09-15 10:30:14 -0700

    commit inicial

---
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controllers;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.Producto;
+import com.example.demo.reposities.ProductRepository;
+
+@RestController
+@RequestMapping(value = "/productos")
+public class ProductController {
+	
+	@Autowired
+	ProductRepository repository;
+	
+	
+
+		
+		
+	}
+
+
